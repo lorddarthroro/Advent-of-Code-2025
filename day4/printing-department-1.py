9ca11b91d3b77e906ea7ff46ec42208bca311692
Rohaan Ahmad <94717608+lorddarthroro@users.noreply.github.com>
2025-12-04 18:43:44 -0500

    Update and rename printing-department-1.py to day4/printing-department-1.py

--- day4/printing-department-1.py
+++ day4/printing-department-1.py
@@ -12,13 +12,11 @@ while True:
     for i in range(len(array_2d)):
         for j in range(len(array_2d[i])):
 
-            # Only consider @ cells
             if array_2d[i][j] != "@":
                 continue
 
             number_of_neighbors = 0
 
-            # Count neighbors
             for di in [-1, 0, 1]:
                 for dj in [-1, 0, 1]:
                     if di == 0 and dj == 0:
@@ -30,16 +28,13 @@ while True:
                         if array_2d[ni][nj] == "@":
                             number_of_neighbors += 1
 
-            # If not enough neighbors → mark THIS cell for removal
             if number_of_neighbors < 4:
                 indexes_to_remove.append((i, j))
                 count += 1
 
-    # Remove all marked cells at once
     for i, j in indexes_to_remove:
         array_2d[i][j] = "."
 
-    # Stop if stable (nobody removed)
     if count == temp_count:
         break
 
